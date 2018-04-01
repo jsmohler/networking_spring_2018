58928846e95d9e266013d38c9b7f520ab172f726
Jordan Mohler <jordan.mohler@du.edu>
2018-04-01 10:28:24 -0700

    Completed Assignment 1

---
+++ assignment1/assignment1.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : assignment1.cpp
+// Author      : Jordan Mohler
+// Version     : 1
+// Description : Assignment 1: Git + Hello World
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World\n"; // prints Hello World!!!
+	return 0;
+}
